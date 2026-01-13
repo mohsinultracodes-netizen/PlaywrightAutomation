69cdde5bb7569a14d86fec9d9bf6add4c91bb18d
mohsinultracodes-netizen <mohsin.ultracodes@gmail.com>
2026-01-13 17:13:55 +0500

    Add simple Playwright test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,10 @@
+// tests/example.spec.js
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('homepage has Playwright in title', async ({ page }) => {
+  // Go to Playwright's homepage
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
+  // Expect the title to contain "Playwright"
   await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
